1743e60f9ec5a35090dc66c57a1e3cb16b28057c
Mario Lazzari <mario.lazzari@gmail.com>
2024-04-28 10:57:01 +0200

    Done lesson 1

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# rust-basic-bb
+# Rust basics by Bek Brace
+
+YouTube [playlist](https://www.youtube.com/playlist?list=PLrOQsSoS-V69UWKxV4FNRJFlHS0DUFQA2)
+
+## Primitive (scalar) Data Types
+
+### Integers
+
+- singed: i8, i16, i32, i64, i128
+- unsigned: u8, u16, u32, u64, u128
+
+### Floats
+
+- f32
+- f64
+
+### Booleans
+
+- true
+- false
+
+### Chars
+
+- char
---
+++ scalars/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // integers
+    let x: i32 = 42;
+    let y: u64 = 100;
+    println!("x = {}, y = {}", x, y);
+
+    // floats
+    let pi: f32 = 3.14;
+    println!("pi = {}", pi);
+
+    // booleans
+    let is_snowing: bool = true;
+    println!("Is snowing? {}", is_snowing);
+
+    // chars
+    let a: char = 'a';
+    println!("a = {}", a);
+}
